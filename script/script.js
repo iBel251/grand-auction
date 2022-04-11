f83982b7b9f03cb41d227e7b65962a37f383b336
iBel251 <ibbimran7@gmail.com>
2022-04-11 17:08:52 +0300

    Fix linter errors

--- script/script.js
+++ script/script.js
@@ -1,18 +1,17 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuBar = document.querySelector('.top-header ul');
-var menuClicked = false;
+let menuClicked = false;
 
 menuBtn.addEventListener('click', () => {
-    console.log('clicked');
-    menuBtn.classList.toggle("fa-xmark");
-    menuBtn.classList.toggle("fa-bars");
-    if (window.innerWidth < 768){
-        if (!menuClicked) {
-            menuBar.style.transform = "translateY(0)";
-            menuClicked = true;
-        } else {
-            menuBar.style.transform = "translateY(-110vh)";
-            menuClicked = false;
-        }
+  menuBtn.classList.toggle('fa-xmark');
+  menuBtn.classList.toggle('fa-bars');
+  if (window.innerWidth < 768) {
+    if (!menuClicked) {
+      menuBar.style.transform = 'translateY(0)';
+      menuClicked = true;
+    } else {
+      menuBar.style.transform = 'translateY(-110vh)';
+      menuClicked = false;
     }
+  }
 });
